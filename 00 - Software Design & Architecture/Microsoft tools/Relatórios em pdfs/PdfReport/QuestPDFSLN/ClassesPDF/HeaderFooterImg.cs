720504cb2c52ecbde0798f2c15bcefee7f9f0b9b
Deyvid Rannyere <deyran@gmail.com>
2026-01-25 20:27:52 -0300

    Finalizado Documento 1

--- 00 - Software Design & Architecture/Microsoft tools/Relatórios em pdfs/PdfReport/QuestPDFSLN/ClassesPDF/HeaderFooterImg.cs
+++ 00 - Software Design & Architecture/Microsoft tools/Relatórios em pdfs/PdfReport/QuestPDFSLN/ClassesPDF/HeaderFooterImg.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Humanizer;
 using System.IO;
 using System.Linq;
 using QuestPDF.Fluent;
@@ -13,21 +14,10 @@ namespace QuestPDFSLN.ClassesPDF
 {
     public class HeaderFooterImg
     {
+        decimal ValorContrato = 200.00m;
 
-        private string TextoConteudo01 = @"CONTRATANTE: _NOME_, _CPF_ CONTRATADO: Espaço Cultural Intellectus Vita LTDA, CNPJ Nº 27.219.858/0001-06. OBJETO DO CONTRATO: Prestação de serviços educacionais sob o título ""Estudo Dirigido"".";
-
-        private string TextoConteudo02 = @"CLÁUSULAS: Objeto do Contrato: O CONTRATANTE contrata o CONTRATADO para a prestação de serviços educacionais denominados ""Estudo Dirigido"", conforme especificado no presente contrato.";
-
-        private string TextoConteudo03 = @"1. Valor: O valor total deste contrato é de R$ 100.00 por disciplina, devidamente pago pelo CONTRATANTE ao CONTRATADO.";
-
-        private string TextoConteudo04 = @"2. Duração: O contrato tem duração de 2 meses, a partir da data estipulada pelo CONTRATADO.";
-
-        private string TextoConteudo05 = @"3. Obrigações do CONTRATADO: O CONTRATADO se compromete a ministrar as aulas conforme o cronograma estipulado pelo CONTRATADO, com a qualidade esperada e dentro dos termos deste contrato.";
-
-        private string TextoConteudo06 = @"4. Obrigações do CONTRATANTE: O CONTRATANTE se compromete a pagar o valor acordado e a cumprir com todas as obrigações estabelecidas neste contrato.";
-
-        private string TextoConteudo07 = @"5. A disciplina(s) a ser ministrada pelo CONTRATADO: Ciências, Físicas e Biológicas (C.F.B), totalizando R$ 100.00.";
-
+        private int _FontSize = 12;
+        private string _FontFamily = "Courier New";
         private string Assinaturas = @"
         ───────────────────────────────────────
         CONTRATANTE
@@ -39,7 +29,7 @@ namespace QuestPDFSLN.ClassesPDF
         ───────────────────────────────────────
         CONTRATADO";
 
-        private string DataContrato = $"Belém, {DateTime.Now:dd} de {DateTime.Now.ToString("MMMM", new CultureInfo("pt-BR"))} de {DateTime.Now:yyyy}";
+        //private string DataContrato = ;
 
         public void gerarDocumento()
         {
@@ -58,21 +48,37 @@ namespace QuestPDFSLN.ClassesPDF
             {
                 Nome = "Deyvid Rannyere de Moraes Costa",
                 CPF = "AAA.AAA.AAA-AA",
-                Disciplinas = new List<string>{ "Ciências", "Físicas", "Biológicas" }
+                Disciplinas = new List<string>
+                { 
+                    "Ciências, Físicas e Biológicas (C.F.B)", 
+                    "Físicas", 
+                    "História" 
+                }
             },
 
             new Pessoa
             {
                 Nome = "Lara Hellena Costa de Moraes",
                 CPF = "BBB.BBB.BBB-BB",
-                Disciplinas = new List<string>{ "Português", "Inglês", "Matemática" }
+                Disciplinas = new List<string>
+                { 
+                    "Língua Portuguesa", 
+                    "Matemática" 
+                }
             },
 
             new Pessoa
             {
                 Nome = "Márcia Costa Silva de Moraes",
                 CPF = "CCC.CCC.CCC-CC",
-                Disciplinas = new List<string>{ "Português", "Inglês", "Matemática" }
+                Disciplinas = new List<string>
+                { 
+                    "Língua Portuguesa", 
+                    "Inglês",
+                    "História",
+                    "Sociologia",
+                    "Matemática" 
+                }
             }
         };
 
@@ -101,133 +107,119 @@ namespace QuestPDFSLN.ClassesPDF
         // --- HEADER ---
         private void criarHeader(PageDescriptor page, string caminhoLogo)
         {
-            page.Header().Column(col =>
+            page.Header().Column(column =>
             {
                 if (File.Exists(caminhoLogo))
-                    col.Item().AlignCenter().Width(80).Image(caminhoLogo);
+                    column.Item().AlignCenter().Width(80).Image(caminhoLogo);
 
-                col.Item().PaddingTop(5);
 
-                col.Item().AlignCenter().Text("CONTRATO ESTUDO DIRIGIDO")
-                    .FontFamily("Courier New")
-                    .FontSize(18)
-                    .SemiBold();
+                column.Item().PaddingTop(20).Text(text =>
+                {
+                    text.DefaultTextStyle(x => x.FontFamily(_FontFamily).FontSize(_FontSize));
+
+                    text.Span("CONTRATO ESTUDO DIRIGIDO")
+                        .Bold()
+                        .FontSize(15);
+
+                    text.AlignCenter();
+                });
+
             });
         }
 
         // --- CONTENT ---
         private void criarContent(PageDescriptor page)
         {
-            page.Content().PaddingVertical(20).PaddingLeft(20).PaddingRight(20).Column(column =>
+            page.Content().PaddingVertical(5).PaddingLeft(20).PaddingRight(20).Column(column =>
             {
 
                 int contarPessoa = 0;
 
                 foreach (var pessoa in ObterPessoas())
                 {
-                    var TextoContrato = TextoConteudo01
-                                        .Replace("_NOME_", pessoa.Nome)
-                                        .Replace("_CPF_", pessoa.CPF);
 
-                    column.Item().Column(itemColumn =>
+                    column.Item().PaddingTop(15).Text(text =>
                     {
-                        itemColumn.Item()
-                            .PaddingTop(15)
-                            .Text(TextoContrato)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
-                    });
+                        text.DefaultTextStyle(x => x.FontFamily(_FontFamily).FontSize(_FontSize));
 
-                    column.Item().Column(itemColumn =>
-                    {
-                        itemColumn.Item()
-                            .PaddingTop(15)
-                            .Text(TextoConteudo02)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
-                    });
+                        text.Span("CONTRATANTE: ").Bold();
+                        text.Span($"{pessoa.Nome}, {pessoa.CPF} ");
 
-                    column.Item().Column(itemColumn =>
-                    {
-                        itemColumn.Item()
-                            .PaddingTop(10)
-                            .PaddingLeft(20)
-                            .Text(TextoConteudo03)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
+                        text.Span("CONTRATADO: ").Bold();
+                        text.Span("Espaço Cultural Intellectus Vita LTDA, CNPJ Nº 27.219.858/0001-06. ");
+
+                        text.Span("OBJETO DO CONTRATO: ").Bold();
+                        text.Span("Prestação de serviços educacionais sob o título \"Estudo Dirigido\".");
+
+
+                        text.Span("\n\nCLÁUSULAS: ").Bold();
+                        text.Span("Objeto do Contrato: O CONTRATANTE contrata o CONTRATADO para a ");
+                        text.Span(" prestação de serviços educacionais denominados \"Estudo Dirigido\", conforme especificado no presente contrato.");
+
+
+                        text.Justify();
                     });
 
 
-                    column.Item().Column(itemColumn =>
+                    column.Item().PaddingTop(15).PaddingLeft(20).Text(text =>
                     {
-                        itemColumn.Item()
-                            .PaddingTop(10)
-                            .PaddingLeft(20)
-                            .Text(TextoConteudo04)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
-                    });
+                        text.DefaultTextStyle(x => x.FontFamily(_FontFamily).FontSize(_FontSize));
 
-                    column.Item().Column(itemColumn =>
-                    {
-                        itemColumn.Item()
-                            .PaddingTop(10)
-                            .PaddingLeft(20)
-                            .Text(TextoConteudo05)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
-                    });
+                        text.Span("1. ");
+                        text.Span("Valor: ").Bold();
+                        text.Span("O valor total deste contrato é de ");
+                        text.Span("R$ " + ValorContrato.ToString("F2")).Bold();
+                        text.Span(" (" + FormatarValorPorExtenso(ValorContrato) + ") ");
+                        text.Span(" por disciplina, devidamente pago pelo CONTRATANTE ao CONTRATADO.");
+                                                
+                        text.Span("\n\n2. ");
+                        text.Span("Duração: ").Bold();
+                        text.Span("O contrato tem duração de 2 meses, a partir da data estipulada pelo CONTRATADO.");
 
-                    column.Item().Column(itemColumn =>
-                    {
-                        itemColumn.Item()
-                            .PaddingTop(10)
-                            .PaddingLeft(20)
-                            .Text(TextoConteudo06)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
-                    });
+                        text.Span("\n\n3. ");
+                        text.Span("Obrigações do CONTRATADO: ").Bold();
+                        text.Span("O CONTRATADO se compromete a ministrar as aulas conforme o cronograma "); 
+                        text.Span(" estipulado pelo CONTRATADO, com a qualidade esperada e dentro dos termos deste contrato.");
 
+                        text.Span("\n\n4. ");
+                        text.Span("Obrigações do CONTRATANTE: ").Bold();
+                        text.Span("O CONTRATANTE se compromete a pagar o valor acordado e a cumprir com ");
+                        text.Span(" todas as obrigações estabelecidas neste contrato.");
 
-                    column.Item().Column(itemColumn =>
-                    {
-                        itemColumn.Item()
-                            .PaddingTop(10)
-                            .PaddingLeft(20)
-                            .Text(TextoConteudo07)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .Justify();
+                        text.Span("\n\n5. ");
+                        text.Span("Disciplina(s): ").Bold();
+                        text.Span("A(s) disciplina(s) a ser ministrada pelo CONTRATADO: ");
+                        text.Span(string.Join(", ", pessoa.Disciplinas)).Bold();
+
+                        decimal valorTotal = ValorContrato * pessoa.Disciplinas.Count();
+                        text.Span(", totalizando ");
+                        text.Span("R$ " + valorTotal.ToString("F2")).Bold();
+                        text.Span(" (" + FormatarValorPorExtenso(valorTotal)  + ")");
+                        text.Span(".");
+
+                        text.Justify();
                     });
 
 
-                    column.Item().Column(itemColumn =>
+                    column.Item().PaddingTop(45).Text(text =>
                     {
-                        itemColumn.Item()
-                            .PaddingTop(35)
-                            .Text(DataContrato)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .AlignRight();
+                        text.DefaultTextStyle(x => x.FontFamily(_FontFamily).FontSize(_FontSize));
+
+                        text.Span($"Belém, {DateTime.Now:dd} de {DateTime.Now.ToString("MMMM", new CultureInfo("pt-BR"))} de {DateTime.Now:yyyy}");
+
+                        text.AlignRight();
                     });
 
 
-                    column.Item().Column(itemColumn =>
+                    column.Item().PaddingTop(55).Text(text =>
                     {
-                        itemColumn.Item()
-                            .PaddingTop(45)
-                            .Text(Assinaturas)
-                            .FontFamily("Courier New")
-                            .FontSize(12)
-                            .AlignCenter();
+                        text.DefaultTextStyle(x => x.FontFamily(_FontFamily).FontSize(_FontSize));
 
+                        text.Span(Assinaturas);
+
+                        text.AlignCenter();
                     });
+
 
                     contarPessoa += 1;
                     if (contarPessoa < ObterPessoas().ToList().Count)
@@ -246,13 +238,39 @@ namespace QuestPDFSLN.ClassesPDF
             {
                 footer
                     .Span("Espaço Cultural Intellectus Vita LTDA Rua Anchieta, 335, Marambaia Belém – Pará – Brasil.")
-                    .FontFamily("Courier New")
+                    .FontFamily(_FontFamily)
                     .FontSize(10);
             });
         }
 
 
         // --- INFRAESTRUTURA E LIMPEZA ---
+        private string FormatarValorPorExtenso(decimal valor)
+        {
+            var cultura = new CultureInfo("pt-BR");
+
+            // 1. Separa reais e centavos
+            long reais = (long)Math.Truncate(valor);
+            int centavos = (int)Math.Round((valor - reais) * 100);
+
+            // 2. Converte para extenso usando cast para long/int (para evitar o erro de 'decimal')
+            string textoReais = reais.ToWords(cultura);
+            string resultado = $"{textoReais} {(reais == 1 ? "real" : "reais")}";
+
+            if (centavos > 0)
+            {
+                string textoCentavos = centavos.ToWords(cultura);
+                resultado += $" e {textoCentavos} {(centavos == 1 ? "centavo" : "centavos")}";
+            }
+
+            // 3. Torna a primeira letra maiúscula
+            // Usando Humanizer:
+            return resultado.Transform(To.SentenceCase);
+
+            // OU usando C# Puro (caso não queira usar o .Transform):
+            // return char.ToUpper(resultado[0]) + resultado.Substring(1);
+        }
+
         private void LimparArquivosTemp()
         {
             string tempDir = Path.GetTempPath();
